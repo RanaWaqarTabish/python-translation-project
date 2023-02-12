d1735617db532ccbfc1a28e90702a5c7c1950f2b
Rana Waqar Tabish <rzt0054@auburn.edu>
2023-02-12 03:17:18 -0600

    This code has to be changed it is not working.

--- translate.py
+++ translate.py
@@ -171,8 +171,27 @@ def get_longest_peptide(rna_sequence, genetic_code):
         A string of the longest sequence of amino acids encoded by
         `rna_sequence`.
     """
-    pass
-
+    rna_sequence = rna_sequence.upper()
+    peptide = ""
+    for i in range(0, len(rna_sequence), 3):
+        codon = rna_sequence[i:i+3]
+        if len(codon) == 3:
+            amino_acid = genetic_code.get(codon, "")
+            if amino_acid == "":
+                return ""
+            peptide += amino_acid
+    return peptide
+def get_longest_peptide(rna_sequence, genetic_code):
+    # Function to get the longest peptide encoded by an RNA sequence
+    peptides = []
+    # Get the reverse and complement of the RNA sequence
+    rna_sequence_rc = reverse_complement(rna_sequence)
+    # Explore the six reading frames
+    for i in range(3):
+        peptides.append(translate_rna(rna_sequence[i:], genetic_code))
+        peptides.append(translate_rna(rna_sequence_rc[i:], genetic_code))
+    # Return the longest peptide
+    return max(peptides, key=len, default="")
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
