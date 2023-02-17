879079830994ff783e48f455b0add6b80b057ddf
Rana Waqar Tabish <rzt0054@auburn.edu>
2023-02-17 11:30:16 -0600

    Changes made to tryto run last code for getting longest peptide

--- translate.py
+++ translate.py
@@ -185,13 +185,14 @@ def get_longest_peptide(rna_sequence, genetic_code):
     # Function to get the longest peptide encoded by an RNA sequence
     peptides = []
     # Get the reverse and complement of the RNA sequence
-    rna_sequence_rc = reverse_complement(rna_sequence)
+    rna_sequence_rc = reverse_and_complement(rna_sequence)
     # Explore the six reading frames
     for i in range(3):
-        peptides.append(translate_rna(rna_sequence[i:], genetic_code))
-        peptides.append(translate_rna(rna_sequence_rc[i:], genetic_code))
+        peptides.append(get_all_translations(rna_sequence, genetic_code))
+        peptides.append(get_all_translations(rna_sequence_rc, genetic_code))
     # Return the longest peptide
-    return max(peptides, key=len, default="")
+    print(peptides)
+    return max(peptides, key=len)
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
